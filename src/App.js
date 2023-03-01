11e6018334ae6c5eba51cf85801b71d0f93fc0dc
Femi Bello <joshuaobello95@gmail.com>
2023-02-28 23:52:45 -0600

    Clear filler code, add skeleton of project.
    1. Work started on timer function
    2. HTML layout mainly done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
 function App() {
+	const [cycle_count, set_cycle_count] = React.useState(0);	
+	const [time_display, set_time_display] = React.useState(0);
+	const timer = time => // TODO: Add callback to cancel timer when starting new one.
+	{
+			let time_left = time;
+			set_time_display(time_left);
+			const clock = setInterval(() => 
+			{
+				if(time_left === 0) clearInterval(clock);	
+				time_left--;
+				set_time_display(time_left);
+			}, 1000);
+	}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<p>{time_display}<sup>{cycle_count}</sup></p>
+			<button onClick={() => timer(1000)} > work </button>
+			<button> break </button>
     </div>
   );
 }
