3f1eaccc2da28ced1093bdab6c8c7f8928f06f1c
Femi Bello <joshuaobello95@gmail.com>
2023-03-01 19:27:36 -0600

    Add timer cancelation.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import * as React from 'react';
 
+
+let current_clock = null;
+
 function App() {
 	const [cycle_count, set_cycle_count] = React.useState(0);	
 	const [time_display, set_time_display] = React.useState(0);
-	const timer = time => // TODO: Add callback to cancel timer when starting new one.
+
+	const timer = time =>
 	{
-			let time_left = time;
+		if(current_clock != null)
+			clearInterval(current_clock);
+		let time_left = time;
+		set_time_display(time_left);
+		const clock = setInterval(() => 
+		{
+			if(time_left === 0) clearInterval(clock);	
+			time_left--;
 			set_time_display(time_left);
-			const clock = setInterval(() => 
-			{
-				if(time_left === 0) clearInterval(clock);	
-				time_left--;
-				set_time_display(time_left);
-			}, 1000);
+		}, 1000);
+		current_clock = clock;
 	}
   return (
     <div className="App">
 			<p>{time_display}<sup>{cycle_count}</sup></p>
-			<button onClick={() => timer(1000)} > work </button>
-			<button> break </button>
+			<button onClick={() => timer(250)} > work </button>
+			<button onClick={() => timer(50)}> break </button>
     </div>
   );
 }
